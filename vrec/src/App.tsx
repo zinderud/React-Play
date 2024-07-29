16c776467896aeb99c87ef2a074a79ef1b93131b
zinderud <mokosam@gmail.com>
2024-07-29 15:13:52 +0300

    vite

---
+++ vrec/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='text-3xl font-bold underline'>App</div>
+    </>
+  )
+}
+
+export default App
